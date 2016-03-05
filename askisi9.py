e0a94e7600de4ffa3a0c8b7a80b7296c6d20ec90
fanis1996 <fanis1996GR20@hotmail.com>
2016-03-05 15:50:01 +0200

    askisi9

--- askisi9.py
+++ askisi9.py
@@ -1,25 +1,48 @@
 from random import randint
-stage = [[" " for x in range(20)] for x in range(10)]
+stage = [[" " for x in range(10)] for x in range(20)]
 c=0
 Play = True
-def addL(x,y):
+def addL(y):
     for i in range(3):
-        stage[x+i][y]="0"
+        stage[i][y]="0"
         if i ==2 :
-            stage[x+2][y+1]="0"
+            stage[i][y+1]="0"
+def removeRow(Row):
+    for i in range(Row+1,0):
+        if i != 0 : stage[i] = stage[i-1] 
+        else : stage[i]={" "," "," "," "," "," "," "," "," "," "}
+def MoveDown(x,y):
+    stage[x][y]=" "
+    stage[x+2][y+1]=" "
+    for i in range(2):
+        stage[x+3][y+i]="0"
+def Col(x,y):
+    if stage[x][y] == "0" or stage[x][y+1] == "0":
+        return True
+    else:
+        return False
 while Play == True:
-    RN = randint(0,18)
-    for i in range(10):
-        if stage[i][RN] == "0" or stage[i][RN+1] == "0":
-            if i-3 < 0 :
-                Play =False
+    RN = randint(0,8)
+    for i in range(20):
+        if i<2:
+            if Col(i,RN)==True:
+                Play = False
                 break
-            else :
-                addL(i-3, RN)
+            else:
+                continue
+        elif i==2:
+            if Col(i, RN) == True:
+                Play = False
+                break
+            else:
                 c+=1
+                addL(RN)
+                continue
+        else:
+            if Col(i, RN) == True:
                 break
-        elif i == 9 :
-            addL(i-3, RN)
-            c+=1
-            break
+            else:
+                MoveDown(i-3, RN)
+        if stage[i] == {"0","0","0","0","0","0","0","0","0","0"}:
+            removeRow(i)
 print c
